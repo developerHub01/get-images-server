2392477418ddd4afece5e66cf969176f035a24e9
shaki <developerhubytchannel@gmail.com>
2023-12-16 18:17:30 +0600

    .

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  res.send("GET IMAGES ===================");
+});
+app.get("/health", async (req, res) => {
+  res.send("server running successfully");
+});
+
 app.get("/fetchData", async (req, res) => {
   const { url } = req.query;
 
